498cf0daf90a13f405b0616d6e285811927d4a0b
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2018-02-11 22:43:07 -0500

    2.4.1 find hero

---
+++ Protactor/tour-of-heroes-cli/e2e/app.e2e-spec.ts
@@ -0,0 +1,51 @@
+import { TourOfHeroesPage } from './app.po';
+
+describe('Tour of heroes Dashboard', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage();
+  });
+
+  it('should display top 4 heroes', () => {
+    page.navigateTo();
+    expect(page.getTop4Heroes()).toEqual(['Mr. Nice', 'Narco', 'Bombasto', 'Celeritas']);
+  });
+
+  it('should navigate to heroes', () => {
+    page.navigateToHeroes();
+    expect(page.getAllHeroes().count()).toBe(11);
+  });
+});
+
+describe('Tour of heroes, heroes page', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+    page.navigateToHeroes();
+  });
+
+  it('should add a new hero', () => {
+    const currentHeroes = page.getAllHeroes().count();
+    page.enterNewHeroInInput('My new Hero');
+    expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n + 1));
+  });
+
+});
+
+describe('Tour of heroes, heroes page, Find Heroe', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+    page.navigateTo();
+  });
+
+  it('should find a hero Dynama', () => {
+    const heroFound = page.heroNameToFind('Zero');
+    expect(heroFound.count()).toBe(1);
+    //expect(heroFound.first().getText()).toBe('Zero details!');
+  });
+
+});
---
+++ Protactor/tour-of-heroes-cli/e2e/app.po.ts
@@ -0,0 +1,32 @@
+//works with html elements
+import {browser, by, element, ElementFinder} from 'protractor';
+
+export class TourOfHeroesPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTop4Heroes() {
+    return element.all(by.css('.module.hero')).all(by.tagName('h4')).getText();
+  }
+
+  navigateToHeroes() {
+    element(by.linkText('Heroes')).click();
+  }
+
+  getAllHeroes() {
+    return element(by.tagName('my-heroes')).all(by.tagName('li'));
+  }
+
+  enterNewHeroInInput(newHero: string) {
+    element(by.tagName('input')).sendKeys(newHero);
+    element(by.buttonText('Add')).click();
+  }
+
+  heroNameToFind(hero: string) {
+    element(by.id('search-box')).sendKeys(hero);
+    element(by.css('.search-result')).click();
+    return element.all(by.tagName('h2'));
+  }
+
+}
