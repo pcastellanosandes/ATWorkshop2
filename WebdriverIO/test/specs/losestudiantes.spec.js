03d309df90c9752490f5ca8b41d88e12e3a1e2d3
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2018-02-12 20:21:29 -0500

    3 webdriverIO configuration working

---
+++ WebdriverIO/test/specs/losestudiantes.spec.js
@@ -0,0 +1,41 @@
+var assert = require('assert');
+describe('los estudiantes login', function() {
+  beforeEach(function() {
+       originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+     });
+
+     afterEach(function() {
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+     });
+
+    it('should visit los estudiantes and failed at log in', function () {
+        browser.url('https://losestudiantes.co');
+
+        browser.click('button=Cerrar');
+        browser.waitForVisible('button=Ingresar', 10000);
+        browser.click('button=Ingresar');
+
+        var cajaLogIn = browser.element('.cajaLogIn');
+        var mailInput = cajaLogIn.element('input[name="correo"]');
+
+        mailInput.click();
+        mailInput.keys('wrongemail@example.com');
+
+        var passwordInput = cajaLogIn.element('input[name="password"]');
+
+        passwordInput.click();
+        passwordInput.keys('1234');
+
+        cajaLogIn.element('button=Ingresar').click()
+        browser.waitForVisible('.aviso.alert.alert-danger', 10000);
+
+        var alertText = browser.element('.aviso.alert.alert-danger').getText();
+        expect(alertText).toBe('Upss! El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.');
+    });
+
+
+  
+
+
+});
