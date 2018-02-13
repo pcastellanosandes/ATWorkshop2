03f40b9aebb8b58739b99e6f192e46803efff9e8
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2018-02-12 23:54:43 -0500

    3.3.1 Login user and create account failed

--- WebdriverIO/test/specs/CreateUser.spec.js
+++ WebdriverIO/test/specs/CreateUser.spec.js
@@ -1,47 +1,47 @@
-// describe('los estudiantes create user with existing information', function() {
-//   beforeEach(function() {
-//        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-//        jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
-//      });
-//
-//      afterEach(function() {
-//        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-//      });
-//
-//     it('should visit los estudiantes and create account has to fail', function () {
-//         browser.url('https://losestudiantes.co');
-//         browser.click('button=Cerrar');
-//         browser.waitForVisible('button=Ingresar', 10000);
-//         browser.click('button=Ingresar');
-//
-//         var cajaSignUp = browser.element('.cajaSignUp');
-//
-//         var nameInput = cajaSignUp.element('input[name="nombre"]');
-//         nameInput.click();
-//         nameInput.keys('Paula');
-//
-//         var lastNameInput = cajaSignUp.element('input[name="apellido"]');
-//         lastNameInput.click();
-//         lastNameInput.keys('Castellanos');
-//
-//         var mailInput = cajaSignUp.element('input[name="correo"]');
-//         mailInput.click();
-//         mailInput.keys('pa.castellanos22@uniandes.edu.co');
-//
-//         cajaSignUp.element('input[name="acepta"]').click();
-//
-//         var passwordInput = cajaSignUp.element('input[name="password"]');
-//         passwordInput.click();
-//         passwordInput.keys('test1234');
-//
-//
-//         cajaSignUp.element('select[name="idPrograma"]').selectByValue('12'); //Ingenieria de sistemas y de computaciòn
-//
-//         browser.click('button=Registrarse');
-//
-//         var alert = browser.waitForVisible('.sweet-alert', 10000);
-//         expect(browser.getTagName('h2=Ocurrió un error activando tu cuenta')).toBe('h2');
-//
-//     });
-//
-// });
+describe('los estudiantes create user with existing information', function() {
+  beforeEach(function() {
+       originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+     });
+
+     afterEach(function() {
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+     });
+
+    it('should visit los estudiantes and create account has to fail', function () {
+        browser.url('https://losestudiantes.co');
+        browser.click('button=Cerrar');
+        browser.waitForVisible('button=Ingresar', 10000);
+        browser.click('button=Ingresar');
+
+        var cajaSignUp = browser.element('.cajaSignUp');
+
+        var nameInput = cajaSignUp.element('input[name="nombre"]');
+        nameInput.click();
+        nameInput.keys('Paula');
+
+        var lastNameInput = cajaSignUp.element('input[name="apellido"]');
+        lastNameInput.click();
+        lastNameInput.keys('Castellanos');
+
+        var mailInput = cajaSignUp.element('input[name="correo"]');
+        mailInput.click();
+        mailInput.keys('pa.castellanos22@uniandes.edu.co');
+
+        cajaSignUp.element('input[name="acepta"]').click();
+
+        var passwordInput = cajaSignUp.element('input[name="password"]');
+        passwordInput.click();
+        passwordInput.keys('test1234');
+
+
+        cajaSignUp.element('select[name="idPrograma"]').selectByValue('12'); //Ingenieria de sistemas y de computaciòn
+
+        browser.click('button=Registrarse');
+
+        var alert = browser.waitForVisible('.sweet-alert', 10000);
+        expect(browser.getTagName('h2=Ocurrió un error activando tu cuenta')).toBe('h2');
+
+    });
+
+});
--- WebdriverIO/test/specs/losestudiantes.spec.js
+++ WebdriverIO/test/specs/losestudiantes.spec.js
@@ -1,37 +1,37 @@
-// var assert = require('assert');
-//
-// describe('los estudiantes login', function() {
-//   beforeEach(function() {
-//        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-//        jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
-//      });
-//
-//      afterEach(function() {
-//        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-//      });
-//
-//     it('should visit los estudiantes and failed at log in', function () {
-//         browser.url('https://losestudiantes.co');
-//         browser.click('button=Cerrar');
-//         browser.waitForVisible('button=Ingresar', 10000);
-//         browser.click('button=Ingresar');
-//
-//         var cajaLogIn = browser.element('.cajaLogIn');
-//         var mailInput = cajaLogIn.element('input[name="correo"]');
-//
-//         mailInput.click();
-//         mailInput.keys('wrongemail@example.com');
-//
-//         var passwordInput = cajaLogIn.element('input[name="password"]');
-//
-//         passwordInput.click();
-//         passwordInput.keys('1234');
-//
-//         cajaLogIn.element('button=Ingresar').click()
-//         browser.waitForVisible('.aviso.alert.alert-danger', 10000);
-//
-//         var alertText = browser.element('.aviso.alert.alert-danger').getText();
-//         expect(alertText).toBe('Upss! El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.');
-//     });
-//
-// });
+var assert = require('assert');
+
+describe('los estudiantes login', function() {
+  beforeEach(function() {
+       originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+     });
+
+     afterEach(function() {
+       jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+     });
+
+    it('should visit los estudiantes and failed at log in', function () {
+        browser.url('https://losestudiantes.co');
+        browser.click('button=Cerrar');
+        browser.waitForVisible('button=Ingresar', 10000);
+        browser.click('button=Ingresar');
+
+        var cajaLogIn = browser.element('.cajaLogIn');
+        var mailInput = cajaLogIn.element('input[name="correo"]');
+
+        mailInput.click();
+        mailInput.keys('wrongemail@example.com');
+
+        var passwordInput = cajaLogIn.element('input[name="password"]');
+
+        passwordInput.click();
+        passwordInput.keys('1234');
+
+        cajaLogIn.element('button=Ingresar').click()
+        browser.waitForVisible('.aviso.alert.alert-danger', 10000);
+
+        var alertText = browser.element('.aviso.alert.alert-danger').getText();
+        expect(alertText).toBe('Upss! El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.');
+    });
+
+});
